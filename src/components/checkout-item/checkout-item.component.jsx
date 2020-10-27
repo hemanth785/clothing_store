10ceeb1423c8dd1d38bf9b73a745f76423adc2e6
Hemanth R H <hemanth.ramachandra@enparadigm.com>
2020-10-27 10:43:25 +0530

    added increase and decreae quantity of items feature in checkout item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import './checkout-item.style.scss';
+import {clearItemFromCart,addItem,removeItem} from '../../redux/cart/cart.actions';
+import {connect} from 'react-redux';
 
-const CheckoutItem = ({cartItem: {name, imageUrl, price, quantity}}) => (
+const CheckoutItem = ({cartItem ,dispatch}) => {
+    const {name, imageUrl, price, quantity} = cartItem;
+    return (
     <div className="checkout-item">
         <div className="image-container">
             <img src={imageUrl} alt='item' />
         </div>
         <span className="name">{name}</span>
-        <span className="quantity">{quantity}</span>
+        <span className="quantity">
+            <div className='arrow' onClick={() => dispatch(removeItem(cartItem))}>&#10094;</div>
+            <span className='value'>{quantity}</span>
+            <div className='arrow' onClick={() => dispatch(addItem(cartItem))}>&#10095;</div>
+        </span>
         <span className="price">{price}</span>
-        <span className="remove-button">&#10005;</span>
+        <span className="remove-button" onClick={() => dispatch(clearItemFromCart(cartItem))}>&#10005;</span>
     </div>
-)
+    )
+}
 
-export default CheckoutItem;
+export default connect()(CheckoutItem);
